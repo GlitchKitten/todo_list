3d78eb6b2d190a73280b889a0d3897bea8fa84e3
glitchkitten <ahaschenburger@gmail.com>
2015-10-18 10:09:48 +0000

    edits now accepted

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,10 +1,23 @@
 Todos.TodoController = Ember.ObjectController.extend({
   actions: {
-    editTodo: function () {
+    editTodo: function() {
       this.set('isEditing', true);
-    }
+    },
+    acceptChanges: function() {
+       this.set('isEditing', false);
+
+       if (Ember.isEmpty(this.get('model.title'))) {
+         this.send('removeTodo');
+       } else {
+         this.get('model').save();
+       }
+     },
+     removeTodo: function () {
+       var todo = this.get('model');
+       todo.deleteRecord();
+       todo.save();
+     }
   },
-  
   isEditing: false,
   
   isCompleted: function(key, value){
